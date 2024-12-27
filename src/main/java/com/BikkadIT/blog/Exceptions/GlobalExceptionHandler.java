fd3e4276422934f2e1046fd9c050d9084cdfff25
Nileshvalhe02gmail.com <nileshvalhe02@gmail.com>
2024-12-27 15:58:46 +0530

    N1:V1/2 Blog-App

--- src/main/java/com/BikkadIT/blog/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/BikkadIT/blog/Exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,19 @@
 package com.BikkadIT.blog.Exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.BikkadIT.blog.Payloads.ApiResponse;
 
+import jakarta.validation.constraints.NotEmpty;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
@@ -19,5 +26,18 @@ public class GlobalExceptionHandler {
 		String message= ex.getMessage();
 		ApiResponse apiResponse= new ApiResponse(message,false);		
 		return new ResponseEntity<ApiResponse>(apiResponse,HttpStatus.NOT_FOUND);		
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String , String>> 
+	handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+		Map<String , String> resp= new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error)->{
+			String fieldName = ((FieldError) error).getField();
+			String message = error.getDefaultMessage();
+			resp.put(fieldName, message);
+		});
+		return new ResponseEntity<Map<String,String>>(resp,HttpStatus.BAD_REQUEST);
+		
 	}
 }
--- src/main/java/com/BikkadIT/blog/Payloads/UserDto.java
+++ src/main/java/com/BikkadIT/blog/Payloads/UserDto.java
@@ -4,7 +4,10 @@ package com.BikkadIT.blog.Payloads;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,13 +19,19 @@ public class UserDto {
 	
 	private int id;
 	
-	@NotNull
+	@NotEmpty()
+	@Size(min=4, message="Username must be minimum 4 characters !!")
 	private String name;
-	@Email
+	
+	@Email(message="Email address is not valid !!")
 	private String email;
-	@NotBlank
+	
+	@NotEmpty()
+	@Size(min = 6, max= 12, message="Password must be minimum of 6 char")
+//	@Pattern (message="Maximum 4 Alphanumariacal")
 	private String password;
-	@NotNull
+	
+	@NotEmpty()
 	private String about;
 
 }
